f0313d4128bb25378542371bc2f346e0ae5e83e7
CAt0mIcS <c.simon.geier@gmail.com>
2025-07-25 16:48:04 +0200

    Fixed LCD 4-bit mode when resetting 6502

--- src/asm/Hello World.s
+++ src/asm/Hello World.s
@@ -27,13 +27,30 @@ lcd:
     ; Set LCD function set to 4-bit data mode with 2 display lines and 5x8 dots format display mode (0b0010-1000)
     ; In 4-bit mode, first the 4 higher order bits are sent and then the 4 lower order bits.
     ; The enable pin is pulsed HIGH after the higher order bits are set and pulsed low again before the lower order bits are set. Then pulsed HIGH/LOW again to latch lower order bits 
-    
-    ; First send one 8-bit sequence (only 4 higher-order bits will be received) which will set the LCD to 4-bit mode
-    lda #%00100010
+
+    ; Reset display to 8-bit mode by sending the higher nibble three times. This is required if we reset the 6502 multiple times without powering it off, as the LCD retains its settings
+    ldy #3
+lcd_init4:
+    lda #(LCD_E_BIT | %00000011)
     sta PORTB
 
-    lda #%00000010
+    ; Remove LCD_E_BIT
+    and #LCD_NOT_E_BIT
     sta PORTB
+    
+    jsr lcd_wait_busy
+    dey
+    bne lcd_init4
+
+    ; Send nibble to turn on 4-bit mode again
+    lda #(LCD_E_BIT | %00000010)
+    sta PORTB
+
+    and #LCD_NOT_E_BIT
+    sta PORTB
+
+    jsr lcd_wait_busy
+    ; From here on, the LCD is in 4-bit mode
 
     ; Send the actual command to display 2 lines with 5x8 dots format in 4-bit mode (0b0010-1000)
     lda #%00101000
@@ -47,58 +64,51 @@ lcd:
     lda #%00000110
     jsr lcd_write_instruction
 
-loop:
     ; Clear display (0b0000-0001)
     lda #%00000001
     jsr lcd_write_instruction
 
-    lda #"H"
+    ; Print the message
+    ldy #0
+message_print_loop:
+    lda message,y
+    beq halt
     jsr lcd_write_char
-    lda #"e"
-    jsr lcd_write_char
-    lda #"l"
-    jsr lcd_write_char
-    lda #"l"
-    jsr lcd_write_char
-    lda #"o"
-    jsr lcd_write_char
-    lda #" "
-    jsr lcd_write_char
-    lda #"W"
-    jsr lcd_write_char
-    lda #"o"
-    jsr lcd_write_char
-    lda #"r"
-    jsr lcd_write_char
-    lda #"l"
-    jsr lcd_write_char
-    lda #"d"
-    jsr lcd_write_char
+    jsr lcd_wait_busy
+    iny
+    jmp message_print_loop
 
-    jmp loop
+
+halt:
+    jmp halt
+
+message: .asciiz "Hello, World!"
 
 lcd_wait_busy:
     lda #%11110000 ; Set higher-order bits of register B to output, the rest to input
     sta DDRB
+    nop
 
     lda #LCD_READ_BIT
     sta PORTB
+    nop
 
 lcd_wait_busy_internal:
     lda #(LCD_READ_BIT | LCD_E_BIT)
     sta PORTB
-    ; nop ; 300ns hold
+    nop
 
     lda PORTB ; Here we should be loading the busy bit + 0x60
     tax
 
     lda #LCD_READ_BIT
     sta PORTB
+    nop
 
     ; We need to pulse E_BIT again due to the 4-bit operation mode
     lda #(LCD_READ_BIT | LCD_E_BIT)
     sta PORTB
-    ; nop ; 300ns hold
+    nop
 
     lda #LCD_READ_BIT
     sta PORTB
@@ -109,6 +119,7 @@ lcd_wait_busy_internal:
 
     lda #%11111111 ; Set register B back to output
     sta DDRB
+    nop
     rts
 
 lcd_write_instruction:
@@ -121,6 +132,7 @@ lcd_write_instruction:
     lsr
     ora #LCD_E_BIT
     sta PORTB
+    nop
 
     ; remove LCD_E_BIT from a register
     and #LCD_NOT_E_BIT
@@ -132,11 +144,13 @@ lcd_write_instruction:
     and #%00001111
     ora #LCD_E_BIT
     sta PORTB
+    nop
 
     ; remove LCD_E_BIT from a register
     and #LCD_NOT_E_BIT
     ; lda lower order bits
     sta PORTB
+    nop
 
     jsr lcd_wait_busy
     rts
@@ -151,25 +165,28 @@ lcd_write_char:
     lsr
     ora #(LCD_E_BIT | LCD_RS_BIT)
     sta PORTB
+    nop
 
-    ; remove LCD_E_BIT and LCD_RS_BIT from a register
-    and #(LCD_NOT_E_BIT & LCD_NOT_RS_BIT)
+    ; remove LCD_E_BIT from A register
+    and #LCD_NOT_E_BIT
     ; lda higher order bits
     sta PORTB
+    nop
 
     txa
     ; lda (LCD_E_BIT | LCD_RS_BIT | lower order bits)
     and #%00001111
     ora #(LCD_E_BIT | LCD_RS_BIT)
     sta PORTB
+    nop
 
-    ; remove LCD_E_BIT and LCD_RS_BIT from a register
-    and #(LCD_NOT_E_BIT & LCD_NOT_RS_BIT)
+    ; remove LCD_E_BIT from A register
+    and #LCD_NOT_E_BIT
     ; lda lower order bits
     sta PORTB
+    nop
 
     rts
-
 
     .org $fffc
     .word reset
